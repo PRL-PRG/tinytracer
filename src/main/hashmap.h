fa701611c2788c3bbd901617674b5b289618d15a
Konrad Siek <konrad.siek@gmail.com>
2018-07-26 16:11:40 +0200

    More changes to hashmap - flexible simple types, return type.

--- src/main/hashmap.h
+++ src/main/hashmap.h
@@ -9,6 +9,8 @@
 #ifndef __HASHMAP_H__
 #define __HASHMAP_H__
 
+#include <stdint.h>
+
 #define MAP_MISSING -3  /* No such element */
 #define MAP_FULL -2 	/* Hashmap is full */
 #define MAP_OMEM -1 	/* Out of Memory */
@@ -18,20 +20,23 @@
  * any_t is a pointer.  This allows you to put arbitrary structures in
  * the hashmap.
  */
-typedef void *any_t;
+typedef void *hashmap_any_t;
+typedef unsigned long hashmap_val_t;
+typedef uintptr_t hashmap_key_t;
+typedef struct {hashmap_val_t value; int status;} hashmap_ret_t;
 
 /*
  * PFany is a pointer to a function that can take two any_t arguments
  * and return an integer. Returns status code..
  */
-typedef int (*PFany)(any_t, any_t);
+typedef int (*hashmap_PFany)(hashmap_val_t);
 
 /*
  * map_t is a pointer to an internally maintained data structure.
  * Clients of this package do not need to know how hashmaps are
  * represented.  They see and manipulate only map_t's.
  */
-typedef any_t map_t;
+typedef hashmap_any_t map_t;
 
 /*
  * Return an empty hashmap. Returns NULL if empty.
@@ -45,28 +50,28 @@ extern map_t hashmap_new();
  * than MAP_OK the traversal is terminated. f must
  * not reenter any hashmap functions, or deadlock may arise.
  */
-extern int hashmap_iterate(map_t in, PFany f, any_t item);
+extern int hashmap_iterate(map_t in, hashmap_PFany f);
 
 /*
  * Add an element to the hashmap. Return MAP_OK or MAP_OMEM.
  */
-extern int hashmap_put(map_t in, /*char**/ unsigned long key, any_t value);
+extern int hashmap_put(map_t in, hashmap_key_t key, hashmap_val_t value);
 
 /*
  * Get an element from the hashmap. Return MAP_OK or MAP_MISSING.
  */
-extern int hashmap_get(map_t in, /*char**/ unsigned long key, any_t *arg);
+extern hashmap_ret_t hashmap_get(map_t in, hashmap_key_t key);
 
 /*
  * Remove an element from the hashmap. Return MAP_OK or MAP_MISSING.
  */
-extern int hashmap_remove(map_t in, /*char**/ unsigned long key);
+extern int hashmap_remove(map_t in, hashmap_key_t key);
 
 /*
  * Get any element. Return MAP_OK or MAP_MISSING.
  * remove - should the element be removed from the hashmap
  */
-extern int hashmap_get_one(map_t in, any_t *arg, int remove);
+//extern int hashmap_get_one(map_t in, any_t *arg, int remove);
 
 /*
  * Free the hashmap
@@ -78,4 +83,4 @@ extern void hashmap_free(map_t in);
  */
 extern int hashmap_length(map_t in);
 
-#endif __HASHMAP_H__
+#endif //__HASHMAP_H__
