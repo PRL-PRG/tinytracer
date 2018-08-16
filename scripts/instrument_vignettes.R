cdb1b3196f45ff57cd480e003e40f2b4a1395eaa
Konrad Siek <konrad.siek@gmail.com>
2018-08-16 15:00:55 +0200

    Debug information and NAs for nonexistant sources.

--- scripts/instrument_vignettes.R
+++ scripts/instrument_vignettes.R
@@ -1,7 +1,10 @@
+#!/usr/bin/env Rscript
+
 suppressPackageStartupMessages(library("dplyr"))
 suppressPackageStartupMessages(library("tibble"))
 suppressPackageStartupMessages(library("stringr"))
 suppressPackageStartupMessages(library("compiler"))
+suppressPackageStartupMessages(library("optparse"))
 
 # Given the name of the package retrieve a list of its vignettes, also including
 # the information about the path of the vignettes' files and source code:
@@ -13,6 +16,8 @@ suppressPackageStartupMessages(library("compiler"))
 #   1 vignette stringr stringr             .../stringr/doc  ../stringr/data .../stringr/doc/stringr.R             142
 #   2 vignette stringr regular-expressions .../stringr/doc  ../stringr/data .../stringr/doc/regular-expressions.R 152
 list_vignettes_for_package <- function(package_name) {
+  # Print debug info.
+  write(paste0("  * ", package_name), stderr())
   
   # Retrieves information about the package's vignettes and returns an
   # packageIQR object. This object contains a few fields: type, title, header,
@@ -36,13 +41,16 @@ list_vignettes_for_package <- function(package_name) {
   source_files <- file.path(source_directories, 
                             paste0(vignette_descriptions[,"Item"], ".R"))
   
+  # If the source file does not exist, set it to NA.
+  source_files[file.exists(source_files) == FALSE] <- NA
+  
   # Create a private little lambda to count lines of code. It opens up the file
   # and counts the number of lines which do not begin with a comment and which
   # are not blank.
   count_lines_of_code <- function(path) 
     if (file.exists(path)) 
       length(!grepl("^[ \t\n]*#", grepl("[^ \t\n]", readLines(path))))
-    else 0
+    else NA
   
   # Apply the counting function to all the source files.
   lines_of_code <- sapply(source_files, count_lines_of_code)
@@ -64,6 +72,10 @@ list_vignettes_for_package <- function(package_name) {
 # each file.
 prepare_working_directory <- function(working_directory, 
                                       source_directory, data_directory) {
+  # Print debug info.
+  write(paste0("  * ", working_directory, 
+               " <- (", source_directory, ", ", data_directory, ")"), 
+        stderr())
 
   # Create a working directory to copy files to.
   dir.create(working_directory, recursive=TRUE)
@@ -90,20 +102,30 @@ prepare_working_directory <- function(working_directory,
 # or may not be necessary for the vignettes to run. This creates a temporary
 # folders and copies all those files so that each vignette has a private copy of
 # each file.
-prepare_working_directories <- function(info)
+prepare_working_directories <- function(info) {
+  write("Prepare working directories: ", stderr())
   apply(info, 1, function(item) 
     prepare_working_directory(
       working_directory = item['working_directory'], 
       source_directory = item['source_directory'], 
       data_directory = item['data_directory']))
+  write("", stderr())
+}
 
 # Reads the source file pointed to by path and instruments all sections that
 # expect errors by wrapping them in a try call.
 instrument_vignette_source <- function(source_path, instrumented_path) { 
+  # If the file does not exist, skip this step.
+  if (!file.exists(source_path) || is.na(source_path))
+    return()
+  
+  # Print debug info.
+  write(paste0("  * ", source_path, " -> ", instrumented_path), stderr())
   
   # Read in the entire source file from the specified path. Now we have a vector
   # of strings, each element is a single line.
-  lines <- readLines(source_path)
+  lines <- if(file.exists(source_path)) readLines(source_path)
+           else                         character(0)
   
   # We iterate through the source. With each line we either transform it in one
   # of several ways or return it as is. The transformation depends on the state
@@ -161,10 +183,13 @@ instrument_vignette_source <- function(source_path, instrumented_path) {
 
 # Instrument source files by wrapping sections where errors might occur with try
 # statements.
-instrument_vignette_sources <- function(info)
+instrument_vignette_sources <- function(info) {
+  write("Instrument vignettes source files: ", stderr())
   apply(info, 1, function(item) 
     instrument_vignette_source(item['source_file'], 
                                item['instrumented_source_file']))
+  write("", stderr())
+}
 
 # Compiles the vignette source file into bytecode and creates another file that
 # can be used to execute the compiled code. In other words, if the vignettes is
@@ -172,23 +197,32 @@ instrument_vignette_sources <- function(info)
 compile_vignette_to_bytecode_and_create_loader <- function(source_path, 
                                                            compiled_path, 
                                                            loader_path) {
-  cmpfile(source_path, compiled_path)
-  loader_source <- paste0("compiler::loadcmp('", compiled_path, "')")
-  write(loader_source, file=loader_path)
+  if (file.exists(source_path)) {
+    cmpfile(source_path, compiled_path, verbose=FALSE)
+    loader_source <- paste0("compiler::loadcmp('", compiled_path, "')")
+    write(loader_source, file=loader_path)
+  }
 }
 
-compile_vignettes_to_bytecode_and_create_loaders <- function(info)
+compile_vignettes_to_bytecode_and_create_loaders <- function(info) {
+  write("Compile vignettes and create loaders: ", stderr())
   apply(info, 1, function(item) 
     compile_vignette_to_bytecode_and_create_loader(item['source_file'], 
                                                    item['compiled_source_file'],
                                                    item['runnable_source_file']))
+  write("", stderr())
+}
 
 # Lists all vignettes for a list of packages. If a list of packages is not
 # provided, it lists vignettes for all installed packages. The list of vignettes
 # includes additional information about each of them. See
 # `list_vignettes_for_package` for more details.
-list_vignettes_for_packages <- function(packages=installed.packages()[, 1]) 
-  do.call(rbind, lapply(packages, list_vignettes_for_package))
+list_vignettes_for_packages <- function(packages=installed.packages()[, 1])  {
+  write("Gather vignette information: ", stderr())
+  info <- do.call(rbind, lapply(packages, list_vignettes_for_package))
+  write("", stderr())
+  info
+}
 
 # Take the information about vignettes we extract from R and add a few
 # additional pieces of information that we will used to preapre files for
@@ -217,7 +251,9 @@ list_vignettes_for_packages <- function(packages=installed.packages()[, 1])
 provide_instrumentation_info <- function(info, 
                                          working_directory_root, 
                                          compile=TRUE) {
-  
+  # Print debug.
+  write("Synthesize vignette instrumentation info\n", stderr())
+
   # Every vignette has its own working directory. Vignette working directories
   # from the same package go in the same parent directory named after the
   # package in the working directory root:
@@ -231,16 +267,21 @@ provide_instrumentation_info <- function(info,
   
   # For every vignette we will spit out some instrumented source code. This will
   # go into the working directory into a file that has the same name as the
-  # original source code file and the extension ".instrumented.R"
-  instrumented_source_files <- file.path(working_directories, 
-                                         paste0(names, ".instrumented.R"))
+  # original source code file and the extension ".instrumented.R". We do not
+  # make an instrumented source path if the file does not exist though.
+  instrumented_source_files <- ifelse(is.na(info$source_file), NA,
+                                      file.path(working_directories, 
+                                         paste0(names, ".instrumented.R")))
                                          
   # If we have compilation turned on, we will produce a byte-code compiled
   # version of the instrumented code. This will go to into the working directory
   # into a file with the same name as the source, but the extension
-  # ".instrumented.Rc". If compilation is turned off this is an NA vector.
+  # ".instrumented.Rc". If compilation is turned off this is an NA vector. We do
+  # not make a compiled path if the file does not exist, returning NA as well.
   compiled_source_files <- (
-    if (compile) file.path(working_directories, paste0(names, ".instrumented.Rc"))
+    if (compile) 
+      ifelse(is.na(info$source_file), NA, 
+             file.path(working_directories, paste0(names, ".instrumented.Rc")))
     else NA
   )
   
@@ -250,8 +291,11 @@ provide_instrumentation_info <- function(info,
   # source, but the extension ".loader.R". If compilation is turned off we
   # simply use the instrumented source file.
   runnable_source_files <- (
-    if (compile) file.path(working_directories, paste0(names, ".loader.R"))
-    else         instrumented_source_files
+    if (compile) 
+      ifelse(is.na(info$source_file), NA, 
+             file.path(working_directories, paste0(names, ".loader.R")))
+    else  
+      instrumented_source_files
   )
   
   # Copy the existing information into a new structure and add the new
@@ -266,6 +310,10 @@ main <- function(packages=installed.packages()[, 1],
                  working_directory_root="/tmp/vignettes", 
                  compile=TRUE) {
   
+  write(paste0("Instrument ", length(packages), " packages: ", 
+               paste0(packages, collapse=" "), "\n"), 
+        stderr())
+  
   info <- list_vignettes_for_packages(packages)
   info <- provide_instrumentation_info(info, working_directory_root, compile)
   
@@ -275,5 +323,33 @@ main <- function(packages=installed.packages()[, 1],
   if (compile)
     compile_vignettes_to_bytecode_and_create_loaders(info)
   
-  info
+  write(paste0("Write executable file information to ", 
+               file.path(working_directory_root, "info.csv")), 
+        stderr())
+  write.csv(info, file=file.path(working_directory_root, "info.csv"), quote = FALSE)
 }
+
+option_list <- 
+  list(make_option(c("-p", "--packages"), 
+                   action="store", 
+                   type="character",
+                   default=paste(installed.packages()[, 1], collapse=" "),
+                   help="A space-separated list of packages to instrument", 
+                   metavar="PACKAGES"),
+       make_option(c("-w", "--working-dir"), 
+                   action="store", 
+                   type="character", 
+                   default="/tmp/vignettes",
+                   help="Working directory to put instrumented code and auxiliary files into",
+                   metavar="WORKING_DIR"),
+       make_option(c("--no-compile"), 
+                   action="store_true", 
+                   default=FALSE,
+                   help="Do not compile vignettes before execution"))
+
+cfg <- parse_args(OptionParser(option_list=option_list),
+                  positional_arguments=TRUE)
+
+main(packages=str_split(cfg$options$packages, " ", simplify=TRUE),
+     working_directory_root=cfg$options$`working-dir`,
+     compile=!cfg$options$`no-compile`)
